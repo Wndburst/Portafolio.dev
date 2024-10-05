a421c90da43071ebb81fb02d92a488f985dd0575
Thomas Riffo <laihtoxd@gmail.com>
2024-10-05 00:23:26 -0300

    Solucion

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from "./components/Header.jsx"
 import Profile from "./components/Profile.jsx"
 import Work from "./components/Work.jsx"
-
+import About from './components/About.jsx'
 function App() {
 
   return (
